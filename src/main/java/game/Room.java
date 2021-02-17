0d6ab634cd97f515446fb665ce74ca06d9a57f9b
Ewan Brain <ewan.brain@gmail.com>
2021-02-17 14:49:33 +0000

    room inital set up, tests passing

--- src/main/java/game/Room.java
+++ src/main/java/game/Room.java
@@ -1,4 +1,28 @@
 package game;
 
+import enums.EnemyType;
+import enums.TreasureType;
+
 public class Room {
+    private String name;
+    private TreasureType treasureType;
+    private EnemyType enemyType;
+
+    public Room(String name, TreasureType treasureType, EnemyType enemyType) {
+        this.name = name;
+        this.treasureType = treasureType;
+        this.enemyType = enemyType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TreasureType getTreasureType() {
+        return treasureType;
+    }
+
+    public EnemyType getEnemyType() {
+        return enemyType;
+    }
 }
--- src/test/java/RoomTest.java
+++ src/test/java/RoomTest.java
@@ -1,6 +1,35 @@
+import enums.EnemyType;
+import enums.TreasureType;
 import game.Room;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class RoomTest {
 
     Room room;
+
+    @Before
+    public void setUp(){
+        room = new Room("Cave", TreasureType.GEM, EnemyType.VAMPIRE);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Cave", room.getName());
+
+    }
+
+    @Test
+    public void hasTreasureType() {
+        assertEquals(TreasureType.GEM, room.getTreasureType());
+
+    }
+
+    @Test
+    public void hasEnemyType() {
+        assertEquals(EnemyType.VAMPIRE, room.getEnemyType());
+
+    }
 }
